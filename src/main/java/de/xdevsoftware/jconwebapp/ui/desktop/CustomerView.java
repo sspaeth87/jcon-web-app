a073a4c9c10eee8472e323a3eee72a653049c767
Sebastian Spaeth <p.xplugin@gmail.com>
2023-06-15 10:29:48 +0200

    update

--- src/main/java/de/xdevsoftware/jconwebapp/ui/desktop/CustomerView.java
+++ src/main/java/de/xdevsoftware/jconwebapp/ui/desktop/CustomerView.java
@@ -3,6 +3,7 @@ package de.xdevsoftware.jconwebapp.ui.desktop;
 
 import java.util.Arrays;
 import java.util.List;
+
 import com.rapidclipse.framework.server.data.renderer.RenderedComponent;
 import com.rapidclipse.framework.server.resources.CaptionUtils;
 import com.rapidclipse.framework.server.ui.filter.FilterComponent;
@@ -11,6 +12,7 @@ import com.vaadin.flow.component.AttachEvent;
 import com.vaadin.flow.component.ComponentEvent;
 import com.vaadin.flow.component.ComponentEventListener;
 import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.grid.ColumnTextAlign;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.data.provider.DataProvider;
@@ -20,9 +22,9 @@ import com.vaadin.flow.server.WebBrowser;
 
 import de.xdevsoftware.jconwebapp.HasTitle;
 import de.xdevsoftware.jconwebapp.model.Customer;
-import de.xdevsoftware.jconwebapp.renderer.MobileCustomerRenderer;
 import de.xdevsoftware.jconwebapp.repository.CustomerRepository;
 import de.xdevsoftware.jconwebapp.ui.MainLayout;
+import de.xdevsoftware.jconwebapp.ui.column.CustomerEditColumn;
 import de.xdevsoftware.jconwebapp.ui.mobile.CustomerMobileView;
 
 
@@ -55,11 +57,16 @@ public class CustomerView extends VerticalLayout implements HasTitle
 		}
 		else
 		{
-			final List<Customer> customers = CustomerRepository.findAll();
-			this.grid.setDataProvider(DataProvider.ofCollection(customers));
-			
-			this.filterComponent.connectWith(this.grid.getDataProvider());
+			this.loadData();
 		}
+	}
+
+	public void loadData()
+	{
+		final List<Customer> customers = CustomerRepository.findAll();
+		this.grid.setDataProvider(DataProvider.ofCollection(customers));
+		
+		this.filterComponent.connectWith(this.grid.getDataProvider());
 	}
 	
 	public boolean isMobileDevice()
@@ -81,6 +88,8 @@ public class CustomerView extends VerticalLayout implements HasTitle
 			.setHeader(CaptionUtils.resolveCaption(Customer.class, "lastname")).setSortable(true);
 		this.grid.addColumn(Customer::getEmail).setKey("email")
 			.setHeader(CaptionUtils.resolveCaption(Customer.class, "email")).setSortable(true);
+		this.grid.addColumn(RenderedComponent.Renderer(CustomerEditColumn::new)).setKey("renderer").setHeader("...")
+			.setSortable(false).setFlexGrow(0);
 		this.grid.setSelectionMode(Grid.SelectionMode.SINGLE);
 		
 		this.filterComponent.connectWith(this.grid.getDataProvider());
